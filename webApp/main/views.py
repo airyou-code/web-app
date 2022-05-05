2b38b82b2a52faa75c60764e534e6a7189517770
Artem Kirillov <holod7272@outlook.com>
2022-05-05 19:10:33 +0200

    add database

--- webApp/main/views.py
+++ webApp/main/views.py
@@ -1,9 +1,16 @@
 import re
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.template.defaulttags import register
+from taskBd.models import taskiq
 
 def index(request):
     return render(request, 'main/index.html')
 
 def test(request):
-    return render(request, 'main/test.html')
+    task1 = taskiq.objects.all()
+    return render(request, 'main/test.html', {'task1:': task1})
+
+
+@register.filter
+def get_range(value):
+    return range(value)
--- webApp/webApp/settings.py
+++ webApp/webApp/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
+    'taskBd',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
