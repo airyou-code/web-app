2bc66a546189df93ae649d922d4105c2159d4e4c
Artem Kirillov <holod7272@outlook.com>
2022-05-06 16:26:35 +0200

    add bd links img

--- webApp/taskBd/models.py
+++ webApp/taskBd/models.py
@@ -4,14 +4,14 @@ from django.db import models
 
 class taskiq(models.Model):
     # name = models.CharField('name', max_length=20)
-    question = models.CharField('question', max_length=20)
-    corAnsver = models.CharField('corAnsver', max_length=20)
-    answer1 = models.CharField('answer1', max_length=20)
-    answer2 = models.CharField('answer2', max_length=20)
-    answer3 = models.CharField('answer3', max_length=20)
-    answer4 = models.CharField('answer4', max_length=20)
-    answer5 = models.CharField('answer5', max_length=20)
-    answer6 = models.CharField('answer6', max_length=20)
+    question = models.CharField('question', max_length=100)
+    corAnsver = models.CharField('corAnsver', max_length=100)
+    answer1 = models.CharField('answer1', max_length=100)
+    answer2 = models.CharField('answer2', max_length=100)
+    answer3 = models.CharField('answer3', max_length=100)
+    answer4 = models.CharField('answer4', max_length=100)
+    answer5 = models.CharField('answer5', max_length=100)
+    answer6 = models.CharField('answer6', max_length=100)
 
     def __str__(self):
         return self.question
--- webApp/taskBd/views.py
+++ webApp/taskBd/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.template.defaulttags import register
 from .models import taskiq
 
 def test2(request):
     task1 = taskiq.objects.all()
     print(task1)
-    return render(request, 'main/index.html', {'task1': task1})
+    return render(request, 'main/test.html', {'task1': task1})
+
+
+
