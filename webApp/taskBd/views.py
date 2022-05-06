4b352787f581050f6a5f92ff7b61286348dff268
Artem Kirillov <holod7272@outlook.com>
2022-05-06 12:26:21 +0200

    f

--- webApp/taskBd/views.py
+++ webApp/taskBd/views.py
@@ -4,4 +4,4 @@ from .models import taskiq
 def test2(request):
     task1 = taskiq.objects.all()
     print(task1)
-    return render(request, 'main/index.html', {'task1:': task1})
+    return render(request, 'main/index.html', {'task1': task1})
